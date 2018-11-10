afd8a9d0d95e102c462078664e00e215f16bc2cd
Anthonyliu86 <andanglyg@msn.com>
2018-11-10 22:20:23 +0800

    backup test code

--- pipeline/scripts/pipeline-utility-steps-test.groovy
+++ pipeline/scripts/pipeline-utility-steps-test.groovy
@@ -47,6 +47,21 @@ pipeline{
 				}
 			}
 		}
+		stage("write into yaml file") {
+			steps{
+				script{
+					def amap = [name: 'Anthony',
+								age : 18,
+								city: 'Beijing',
+								isMale: true
+								]
+					yaml_file = env.WORKSPACE + "/testdata/new.yml"
+					model_test.write_to_yaml(amap, yaml_file)
+					println "the contents of yaml file are: "
+					model_test.read_yaml_file(yaml_file)
+				}
+			}
+		}
 		
 	}
 }
