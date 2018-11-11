f6eb9281effc4449f44d734236060a8eae1bbcf3
Anthonyliu86 <andanglyg@msn.com>
2018-11-11 21:04:28 +0800

    fix dump stage name

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -7,22 +7,19 @@ pipeline{
 	
 	agent any
 	stages{
-		stage("dir") {
+		stage("dir_echo_error_demo") {
 			steps{
-				script{
-					println env.WORKSPACE
-					sh("ls -al ${env.WORKSPACE}")
-					dir("${env.WORKSPACE}/testdata")
-					deleterDir()
-					sh("ls -al ${env.WORKSPACE}")
-				}
-			}
-		}
-		stage("dir") {
-			steps{
-				println env.WORKSPACE
-				dir("${env.WORKSPACE}/testdata"){
-					sh "pwd"
+			    println env.WORKSPACE
+			    dir("${env.WORKSPACE}/testdata"){
+				    sh "pwd"
+			    }
+			    echo ("list all files under current workd directory")
+			    sh("ls -al ${env.WORKSPACE}")
+				def a = 9
+				if(a == 10) {
+					echo("a equal to 10")
+				}else {
+					error("a is not 10, but a should equal to 10")
 				}
 			}
 		}
