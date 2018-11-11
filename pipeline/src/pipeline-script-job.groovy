16a1beae20b7ec42e36195d31801618f740e9a04
Anthonyliu86 <andanglyg@msn.com>
2018-11-11 21:08:46 +0800

    debug

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -15,12 +15,8 @@ pipeline{
 			    }
 			    echo ("list all files under current workd directory")
 			    sh("ls -al ${env.WORKSPACE}")
-				def a = 9
-				if(a == 10) {
-					echo("a equal to 10")
-				}else {
-					error("a is not 10, but a should equal to 10")
-				}
+				echo("some info logs output")
+				error("some error method output")
 			}
 		}
 	}
