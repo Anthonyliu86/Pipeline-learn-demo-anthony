89af04190f82909d936a08b71d5056be89f5dcb9
Anthonyliu86 <andanglyg@msn.com>
2018-11-13 22:38:52 +0800

    backup send mail post code

--- pipeline/scripts/pipeline-basic-steps-demo.groovy
+++ pipeline/scripts/pipeline-basic-steps-demo.groovy
@@ -33,6 +33,18 @@ pipeline{
 			}
 		}
 	}
+	post{
+		failure {
+			script {
+				module_test.send_email_results("Failed","Master","571072220@qq.com,904194906@qq.com")
+			}
+		}
+		success {
+			script {
+				module_test.send_email_results("Success","Master","2048398933@qq.com")
+			}
+		}
+	}
 }
 
 
