f3f6dec261541563ac757e2a63981b391f5f8036
Anthony_tester <andanglyg@msn.com>
2018-11-13 15:24:14 +0800

    Update pipeline-demo-module.groovy

--- pipeline/module/pipeline-demo-module.groovy
+++ pipeline/module/pipeline-demo-module.groovy
@@ -63,8 +63,8 @@ def write_to_yaml(map_data, yaml_path) {
 
 def send_email_results(status,GITBranch,to_email_address_list) {
     def fileContents = readFile env.WORKSPACE + '/testdata/basic_style.css'
-    def subject = "Jenkins Job : " + env.JOB_NAME + "/" + env.BUILD_ID + "has " +  status
-    def result_url = RUN_DISPLAY_URL + ""
+    def subject = "Jenkins Job : " + env.JOB_NAME + "/" + env.BUILD_ID + " has " +  status
+    def result_url = env.BUILD_URL + "console"
     
 
     def text = """
