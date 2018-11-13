6fd5d9a59ed5668bea516fb6fff1fcd9ed0635d1
Anthony_tester <andanglyg@msn.com>
2018-11-13 14:48:58 +0800

    add send mail method

--- pipeline/module/pipeline-demo-module.groovy
+++ pipeline/module/pipeline-demo-module.groovy
@@ -61,4 +61,38 @@ def write_to_yaml(map_data, yaml_path) {
 	writeYaml file: yaml_path , data: map_data
 }
 
+def send_email_results(status,GITBranch,to_email_address_list) {
+    def fileContents = readFile 'pipeline/testdata/base-style.css'
+    def subject = "Jenkins Job : " + env.JOB_NAME + "/" + env.BUILD_ID + "has " +  status
+    def result_url = RUN_DISPLAY_URL + ""
+    
+
+    def text = """
+  <html>
+  <style type="text/css">
+  <!--
+  ${fileContents}
+  -->
+  </style>
+  <body>
+  <div id="content">
+  <h1>Summary</h1>
+  <div id="sum2">
+      <h2>Jenkins Build</h2>
+      <ul>
+      <li>Job URL : <a href='${env.BUILD_URL}'>${env.BUILD_URL}</a></li>
+       <li>Build Result URL : <a href='${result_url}'>${result_url}</a></li>
+      </ul>
+  </div>
+  <div id="sum0">
+  <h2>GIT Branch</h2>
+  <ul>
+  <li>${GITBranch}</li>
+  </ul>
+  </div>
+  </div></body></html>
+  """
+
+    email body: text, subject: subject,  mimeType: 'text/html', to: to_email_address_list
+}
 return this;
