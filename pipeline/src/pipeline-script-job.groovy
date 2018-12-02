4abe7ec1dba5ffd25f258347b8f9594377eefb3f
Anthonyliu86 <andanglyg@msn.com>
2018-12-02 21:20:53 +0800

    add git check demo

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -7,26 +7,20 @@ pipeline{
 	
 	agent any
 	stages{
-		stage("writeFile demo") {
+		stage("Hello Demo") {
 			steps{
 				script {
-					write_file_path = "${env.WORKSPACE}/testdata/write.txt"
-					file_contents = "Hello Anthony!! 这是一个测试例子"
-					//write into write.txt
-					writeFile file: write_file_path, text: file_contents, encoding: "UTF-8"
-					// read file and print it out
-					fileContents = readFile file: write_file_path, encoding: "UTF-8"
-					println fileContents
+					println "Hello Demo!"
 				}
 			}
 		}
 		
-		stage("catchError demo") {
+		stage("git checkout") {
 			steps{
-				catchError {
-					fileContents = readFile file: "hello.txt", encoding: "UTF-8"
-					println fileContents
-					step (echo "here had met some exections, please check")	
+				script {
+					checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
+							url: 'https://github.com/Anthonyliu86/HelloWorld.git']]])
 				}
 			}
 		}
