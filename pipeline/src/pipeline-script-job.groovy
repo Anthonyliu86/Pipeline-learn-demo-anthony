9f6ed244c42cd10aab434cc1c23c02bfe029ed1d
Anthonyliu86 <andanglyg@msn.com>
2018-12-10 20:45:03 +0800

    check file download

--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -7,13 +7,6 @@ pipeline{
 	
 	agent any
 	stages{
-		stage("Hello Demo") {
-			steps{
-				script {
-					println "Hello Demo!"
-				}
-			}
-		}
 		
 		stage("git checkout") {
 			steps{
@@ -21,6 +14,14 @@ pipeline{
 					checkout([$class: 'GitSCM', branches: [[name: '*/master']],
 						userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
 							url: 'https://github.com/Anthonyliu86/HelloWorld.git']]])
+				}
+			}
+		}
+		stage("Check file download") {
+			steps {
+				script {
+					out = sh(script: "[ -f /tmp/test/Python-3.7.1.tgz]", returnStdout: true)
+					println out
 				}
 			}
 		}
